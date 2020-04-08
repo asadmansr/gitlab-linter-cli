b2002818117d7ab0ec85a85190cb84a4da2c8483
asadmansr <asadmansr@gmail.com>
2020-04-07 21:48:54 -0400

    Refactored validation for URL to remove trailing slashes and only support url that is provided by gitlab api

--- src/utils.js
+++ src/utils.js
@@ -20,24 +20,7 @@ export function validateHost(host) {
 
     if (!isURLValid) {
         console.error(
-            `${chalk.redBright(`Error: URL is not valid`)}
-            `
-        );
-        console.warn(
-            `Please use the command ${chalk.green('gitlab-linter config --host=https://gitlab.com')} to get started.`
-        );
-        console.log(
-            `${chalk.gray('If your GitLab instance has a different URL, configure the host parameter as --host=https://gitlab.example.com')}
-            `
-        );
-        return false;
-    }
-
-    const isGitLabURL = validateGitLabURL(host);
-
-    if (!isGitLabURL) {
-        console.error(
-            `${chalk.redBright(`Error: URL specified does not contain gitlab.com`)}
+            `${chalk.redBright(`Error: URL is not valid or does not contain gitlab domain name.`)}
             `
         );
         console.warn(
@@ -53,16 +36,11 @@ export function validateHost(host) {
     return true;
 }
 
-function validateURL(url) {
-    var pattern = new RegExp('^(https?:\\/\\/)?' +
-        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
-        '((\\d{1,3}\\.){3}\\d{1,3}))' +
-        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
-        '(\\?[;&a-z\\d%_.~+=-]*)?' +
-        '(\\#[-a-z\\d_]*)?$', 'i');
-    return !!pattern.test(url);
+export function formatHost(host) {
+    return host.replace(/\/+$/, "");
 }
 
-function validateGitLabURL(url) {
-    return url.includes("gitlab");
+function validateURL(url) {
+    var pattern = new RegExp('https:\/\/gitlab\.?.*\.[a-z]{2,3}', 'i');
+    return !!pattern.test(url);
 }
